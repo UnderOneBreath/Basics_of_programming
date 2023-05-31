9bd5dcbce4ba08a057fcd05d802a64b3e01bd6d4
Краузе Кейсер <UnderOneBreath>
2023-05-31 12:25:27 +0900

    lol

--- LAB_4/B4.cpp
+++ LAB_4/B4.cpp
@@ -2,19 +2,40 @@
 #include <string>
 using namespace std;
 
-void RecTriangle(int n) {
-    if (n == 0) return;
-    if (n < n/2)
-        RecTriangle(n - 1);
-    for (int i = 0; i < n; i++) {
-        cout << "x";
-    }
-    cout << endl;
-    RecTriangle(n - 1);
+void RegTriangle(int n,int c){
+	if(c==0)
+		{cout<<"*"<<endl;}
+	if(c==n)
+		{return;}
+	else
+	{
+	cout<<"*";	
+	for(int i=0;i<c;++i){
+		{cout<<"x";}
+	}
+	}
+	cout<<"*"<<endl;
+	RegTriangle(n,c+1);
+	
+}
+void RecTriangle(int n){
+	cout<<"*";
+	if(n<0)
+		{return;}
+	else
+	{
+	for(int i=0;i<n;++i){
+		{cout<<"x";}
+	}
+	}
+	cout<<"*"<<endl;
+	RecTriangle(n-1);
 }
 
 int main() {
     int n = 4;
+    int count = 0;
+    RegTriangle(n, count);
     RecTriangle(n);
     return 0;
 }
