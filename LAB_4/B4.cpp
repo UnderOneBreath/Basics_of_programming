71f1b6343a7181412dea97abe6398e9d34b89b19
Innokenty <memories53@mail.ru>
2023-06-09 09:56:56 +0300

    add in file B4 randomNums

--- LAB_4/B4.cpp
+++ LAB_4/B4.cpp
@@ -2,72 +2,38 @@
 #include <string>
 using namespace std;
 
-void RegTriangle(int n,int c){
-	if(c==0)
-		{cout<<"*"<<endl;}
-	if(c==n)
-		{return;}
+void RegTriangle(int n, int c)
+{
+	if (c == 0)
+		cout << "*" << endl;
+	if (c == n)
+		return;
 	else
 	{
-	cout<<"*";	
-	for(int i=0;i<c;++i){
-		{cout<<"x";}
+		cout << "*";
+		for (int i = 0; i < c; ++i)
+			cout << "x";
 	}
-	}
-	cout<<"*"<<endl;
-	RegTriangle(n,c+1);
-	
+	cout << "*" << endl;
+	RegTriangle(n, c + 1);
 }
-void RecTriangle(int n){
-	cout<<"*";
-	if(n<0)
-		{return;}
+void RecTriangle(int n)
+{
+	cout << "*";
+	if (n < 0)
+		return;
 	else
-	{
-	for(int i=0;i<n;++i){
-		{cout<<"x";}
-	}
-	}
-	cout<<"*"<<endl;
-	RecTriangle(n-1);
+		for (int i = 0; i < n; ++i)
+			cout << "x";
+	cout << "*" << endl;
+	RecTriangle(n - 1);
 }
 
-int main() {
-    int n = 4;
-    int count = 0;
-    RegTriangle(n, count);
-    RecTriangle(n);
-    return 0;
+int main()
+{
+	int n = 4;
+	int count = 0;
+	RegTriangle(n, count);
+	RecTriangle(n);
+	return 0;
 }
-
-// void RecTriangle(int n)
-// {
-//     int m = 0;
-//     if (n <= 0)
-//     {
-//         for (int i = 0; i < 4; i++) {
-//             if ((i == 1) || (i == 2))
-//                 cout << string(2, '*') << endl;
-//             else
-//                 cout << "*" << endl;
-//         }
-//         // string lineStar = "*";
-//         // m = n + 4;
-//         // if (m == 0)
-//         //     return;
-//         // if (m % 2 != 0 || m == 2) {
-//         //     cout << lineStar << lineStar << endl;
-//         //     RecTriangle(n - 1);
-//         // } else {
-//         //     if (n = -3) {
-//         //         cout << lineStar << endl;
-//         //         RecTriangle(n - 1);
-//         //     }
-//         //     cout << lineStar << endl;
-//         //     RecTriangle(n - 1);
-//         // }
-//     }
-//     if (n == 0)
-//         return;
-//     if ((n * 2 + 1) )
-// }
