18ba76ebce281bb0581babfadf194b980bca8c9a
Innokenty <memories53@mail.ru>
2023-11-28 07:14:35 +0300

    lab4b

--- SAOD/LAB4b.cpp
+++ SAOD/LAB4b.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <string.h>
+#include <fstream>
 using namespace std;
+
+string input = "input.txt", output = "output.txt";
 
 void res(int *arr)
 {
@@ -18,22 +22,31 @@ void copyArr(int *arr, int *arr2)
     cout << sizeArr1 << ' ' << sizeArr2 << endl;
 }
 
+int fact(int lenght)
+{
+}
+
 int main()
 {
-    int arr[]{1, 5, 10, 50, 100, 500};
-    res(arr);
-    int n = 5;
-    int *arr2 = new int[n];
-    cout << "Size Array dinamic: " << sizeof(arr) << endl;
-    cout << "Size Array dinamic with using *arr: " << sizeof(arr2) << endl;
+    const char line = "ABC";
+    int len;
 
-    copyArr(arr, arr2);
+    len = strlen(line);
 
-    int c;
-    for (auto i : arr)
-    {
-        cout << i << " ";
-        c++;
-    }
-    cout << "\nThe lenght of the given Array is: " << c;
+    // int arr[]{1, 5, 10, 50, 100, 500};
+    // res(arr);
+    // int n = 5;
+    // int *arr2 = new int[n];
+    // cout << "Size Array dinamic: " << sizeof(arr) << endl;
+    // cout << "Size Array dinamic with using *arr: " << sizeof(arr2) << endl;
+
+    // copyArr(arr, arr2);
+
+    // int c;
+    // for (auto i : arr)
+    // {
+    //     cout << i << " ";
+    //     c++;
+    // }
+    // cout << "\nThe lenght of the given Array is: " << c;
 }
