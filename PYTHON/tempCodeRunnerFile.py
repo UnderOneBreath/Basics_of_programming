eb082789ee70e836ae95b00bfe0895fde4e5096a
Краузе Кейсер <UnderOneBreath>
2023-11-28 09:35:53 +0900

    add files TD

---
+++ PYTHON/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+print(timeInNum1, timeInNum2)
+
+# def calculateTime():
+#     if int(timeInNum1[0]) > int(timeInNum2[0]):
+#         f = 24 - int(timeInNum1[0])
+#         time = f + int(timeInNum2[0])
+#     print(time)
+
+
+# k = int(timeInNum2[0]) - int(timeInNum1[0])
+# print(k)
+# calculateTime()
---
+++ SAOD/LAB4b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    fstream input("input.txt");
+    ofstream output("output.txt");
+
+    string n;
+    input >> n;
+    
+    
+}
