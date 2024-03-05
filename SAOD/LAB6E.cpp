fa23849ec859aa9966e6269c536d7b14c97227ee
UnderOneBreath <63227827+UnderOneBreath@users.noreply.github.com>
2024-03-05 15:33:48 +0900

    Update LAB6E.cpp

--- SAOD/LAB6E.cpp
+++ SAOD/LAB6E.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 
 string input = "input.txt", output = "output.txt";
+fstream inp(input);
+ofstream out(output);
 
 double distance(double x1, double y1, double x2, double y2) {
     double d;
@@ -14,8 +16,24 @@ double distance(double x1, double y1, double x2, double y2) {
     return d;
 }
 
-void Check_Parallelism() {
-    
+void Check_Parallelism(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+    //Ax+Bx+C=0
+    double A1, B1, A2, B2;
+    //first line
+    A1 = y2 - y1;
+    B1 = x1 - x2;
+    //second line
+    A2 = y4 - y3;
+    B2 = x3 - x4;
+
+    if ((A1 / A2) == (B1 / B2)) {
+        out << distance(x1, y1, x3, y3);
+        exit(0);
+    }
+}
+
+void Check_Intersections(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+    if (x1)
 }
 
 // double DistFromPointToLine(double px, double py, double x1, double y1, double x2, double y2) {
@@ -23,8 +41,6 @@ void Check_Parallelism() {
 // }
 
 int main() {
-    fstream inp(input);
-    ofstream out(output);
 
     double x1, y1, x2, y2;
     double x3, y3, x4, y4;
