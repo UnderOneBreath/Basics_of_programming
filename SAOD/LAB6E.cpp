89755dcd0f38cf986db8673ecc79eb2df1fdefe5
UnderOneBreath <63227827+UnderOneBreath@users.noreply.github.com>
2024-03-05 15:06:47 +0900

    new mass in distances and delete suckers double void

--- SAOD/LAB6E.cpp
+++ SAOD/LAB6E.cpp
@@ -8,23 +8,20 @@ using namespace std;
 
 string input = "input.txt", output = "output.txt";
 
-double dist(double x1, double y1, double x2, double y2) {
+double distance(double x1, double y1, double x2, double y2) {
     double d;
     d = pow(pow(x2-x1, 2) + pow(y2-y1, 2), 0.5);
     return d;
 }
 
-double distPointToPr(double px, double py, double x1, double y1, double x2, double y2) {
-    double l = pow(x1-x2, 2) + pow(y1-y2, 2);
-    if (l == 0) return dist(px, py, x1, y1);
-
-    double t = max(0.0, min(1.0, ((px-x1) * (x2-x1) + (py-y1) * (y2 - y1))/l));
-    double projX = x1 + t * (x2 - x1);
-    double projY = y1 + t * (y2 - y1);
+void Check_Parallelism() {
+    
 }
 
-// float checkParallelism(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+// double DistFromPointToLine(double px, double py, double x1, double y1, double x2, double y2) {
+    
 // }
+
 int main() {
     fstream inp(input);
     ofstream out(output);
@@ -32,14 +29,20 @@ int main() {
     double x1, y1, x2, y2;
     double x3, y3, x4, y4;
 
-    //Ax + By + C = 0
-    // float a1, b1, c1;
-    // float a2, b2, c2;
-
-    cout distPointToPr()
     inp >> x1 >> y1 >> x2 >> y2;
     inp >> x3 >> y3 >> x4 >> y4;
 
-    cout << dist(x1,y1, x3,y3);
+    double distances [] = {
+        distance(x1, y1, x3, y3),
+        distance(x1, y1, x4, y4),
+        distance(x2, y2, x3, y3),
+        distance(x2, y2, x4, y4),
 
+    };
+
+    double minDist = distances[0];
+    for (int i; i < sizeof(distances); i++)
+        if (distances[i] < minDist)
+            minDist = distances[i];
+    
 }
