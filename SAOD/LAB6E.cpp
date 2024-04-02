13b826b6cbd680c56aa54c2f162a038bd9505fa9
UnderOneBreath <63227827+UnderOneBreath@users.noreply.github.com>
2024-04-02 15:35:37 +0900

    Update LAB6E.cpp added function distanceBetweenParallelLines() and  LineSegmentsIntersection()

--- SAOD/LAB6E.cpp
+++ SAOD/LAB6E.cpp
@@ -11,6 +11,7 @@ fstream inp(input);
 ofstream out(output);
 
 double distance(double x1, double y1, double x2, double y2) {
+    cout << "WORK: Function distance()" << endl;
     double d;
     d = pow(pow(x2-x1, 2) + pow(y2-y1, 2), 0.5);
     return d;
@@ -42,6 +43,7 @@ double point_to_distance(double x, double y, double A, double B, double C) {
 bool LineSegmentsIntersection(double x1, double y1, double x2, double y2, 
     double x3, double y3, double x4, double y4)
 {
+    cout << "WORK: Function LineSegmentsIntersection()" << endl;
     double _1 = x1 - x2;
     double _2 = x1 - x3;
     double _3 = x3 - x4;
@@ -52,6 +54,20 @@ bool LineSegmentsIntersection(double x1, double y1, double x2, double y2,
     double t = (_2 * _6 - _5 * _3) / d;
     double u = (_2 * _4 - _5 * _1) / d;
     return t >= 0. && t <= 1. && u >= 0. && u <= 1.;
+}
+
+double distanceBetweenParallelLines(double a1, double b1, double c1, double a2, double b2, double c2) {
+    cout << "WORK: Function distanceBetweenParallelLines()" << endl;
+    double x, y;
+    if ((b1 * a2 - b2 * a1) && a1) {
+        y = (c2*a1 - c1*a2) / (b1*a2 - b2*a1);
+        x = (-c1 - b1*y) / a1;
+    }
+    else if (b1*a2 - b2*a1 && a2) {
+        y = (c2*a1 - c1*a2) / (b1*a2 - b2*a1);
+        x = (-c2 - b2*y) / a2;
+    }
+    return distance(x, y, 0, 0);
 }
 
 int main() {
@@ -68,6 +84,7 @@ int main() {
     A1 = y1 - y2;
     B1 = x2 - x1;
     C1 = (-1)* A1 * x1 - B1 * y1;
+    if (C1 == -0) C1 == 0;
     //second line
     A2 = y3 - y4;
     B2 = x4 - x3;
@@ -85,18 +102,22 @@ int main() {
     for (int i = 0; i < N; i++)
         if (distances[i] < minDist)
             minDist = distances[i];
-    
+    // out << A1 << ' ' << B1 << ' ' << C1 << ' ' << A2 << ' ' << B2 << ' ' << C2 << endl;
+
     // out << A2 << ' ' << B2 << ' ' << C2;
     if ((A1 / A2) == (B1 / B2) || ((A1 == 0 && A2 == 0) && (B1 / B2)) || ((B1 == 0 && B2 == 0) && (A1 / A2))) {
         // for (int i = 0; i < N; i++)
         //     out << distances[i] << ' ';
-        out << minDist;
+        cout << "Check parallelism" << endl;
+        out << distanceBetweenParallelLines(A1, B1, C1, A2, B2, C2);
         exit(0);
     }
     // Check_Intersections(x1, y1, x2, y2, x3, y3, x4, y4, B1, B2, A1, A2, C1, C2);
-    out << LineSegmentsIntersection(x1, y1, x2, y2, x3, y3, x4, y4);
+    if (LineSegmentsIntersection(x1, y1, x2, y2, x3, y3, x4, y4) == true){
+        cout << "Line Segment Intersection" << endl;
+        out << 0;
+        exit(0);
+    }
 
-    // if (point_to_distance(x1, y1, A2, B2, C2) < point_to_distance(x2, y2, A2, B2, C2))
-    //     out << point_to_distance(x1, y1, A2, B2, C2);
-    // else out << point_to_distance(x2, y2, A2, B2, C2);
+    out << minDist << endl;
 }
